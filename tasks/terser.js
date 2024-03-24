74a278b7e5a709508049ba9f7e0b15e7d265316c
Niels Leenheer <niels@leenheer.nl>
2024-03-24 17:07:44 +0100

    The latest versions of Terser can't run multitple files at the same time, as options of earlier files will be overwritten by later files.

--- tasks/terser.js
+++ tasks/terser.js
@@ -32,66 +32,64 @@ module.exports = (
       let createdFiles = 0;
 
       // Iterate over all specified file groups.
-      await Promise.all(
-        this.files.map(async (file) => {
-          // Concat specified files.
-          const src = file.src
-            .filter((filepath) => {
-              // Warn on and remove invalid source files (if nonull was set).
-              if (!grunt.file.exists(filepath)) {
-                grunt.log.warn(`Source file "${filepath}" not found.`);
-                return false;
-              }
-              return true;
-            })
-            .reduce(
-              (sources, filepath) => ({
-                ...sources,
-                [filepath]: grunt.file.read(filepath),
-              }),
-              {},
-            );
+      for (const file of this.files) {
+        // Concat specified files.
+        const src = file.src
+          .filter((filepath) => {
+            // Warn on and remove invalid source files (if nonull was set).
+            if (!grunt.file.exists(filepath)) {
+              grunt.log.warn(`Source file "${filepath}" not found.`);
+              return false;
+            }
+            return true;
+          })
+          .reduce(
+            (sources, filepath) => ({
+              ...sources,
+              [filepath]: grunt.file.read(filepath),
+            }),
+            {},
+          );
 
-          if (options.sourceMap) {
-            const destination = file.dest.split('/').pop();
+        if (options.sourceMap) {
+          const destination = file.dest.split('/').pop();
 
-            const mapFileName = options.sourceMap.filename
-              ? options.sourceMap.filename
-              : `${destination}.map`;
+          const mapFileName = options.sourceMap.filename
+            ? options.sourceMap.filename
+            : `${destination}.map`;
 
-            options.sourceMap.url = mapFileName;
-          }
+          options.sourceMap.url = mapFileName;
+        }
 
-          // Minify file code.
-          const result = await minify(src, options);
+        // Minify file code.
+        const result = await minify(src, options);
 
-          if (result.error) {
-            grunt.log.error(result.error);
-            return false;
-          }
+        if (result.error) {
+          grunt.log.error(result.error);
+          return false;
+        }
 
-          if (result.warnings) {
-            grunt.log.warn(result.warnings.join('\n'));
-          }
+        if (result.warnings) {
+          grunt.log.warn(result.warnings.join('\n'));
+        }
 
-          // Write the destination file.
-          grunt.file.write(file.dest, result.code);
+        // Write the destination file.
+        grunt.file.write(file.dest, result.code);
 
-          if (options.sourceMap) {
-            const mapFileName = options.sourceMap.filename
-              ? options.sourceMap.filename
-              : `${file.dest}.map`;
-            // Write the source map file.
-            grunt.file.write(mapFileName, result.map);
-          }
+        if (options.sourceMap) {
+          const mapFileName = options.sourceMap.filename
+            ? options.sourceMap.filename
+            : `${file.dest}.map`;
+          // Write the source map file.
+          grunt.file.write(mapFileName, result.map);
+        }
 
-          // Print a success message for individual files only if grunt is run with --verbose flag
-          grunt.log.verbose.writeln(`File "${file.dest}" created.`);
+        // Print a success message for individual files only if grunt is run with --verbose flag
+        grunt.log.verbose.writeln(`File "${file.dest}" created.`);
 
-          // Increment created files counter
-          createdFiles += 1;
-        }),
-      );
+        // Increment created files counter
+        createdFiles += 1;
+      }
 
       if (createdFiles > 0) {
         grunt.log.ok(
